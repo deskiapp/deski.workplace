8c6d4236d5003acedebbe5b671e33fa6582ae95a
sanigasani <86219056+sanigasani@users.noreply.github.com>
2021-09-22 22:21:18 -0700

    done

--- src/Space/Space.js
+++ src/Space/Space.js
@@ -54,43 +54,41 @@ function Space() {
     });
     return (
         <div>
-            <div className="b">
-                <div className="space_sidebar">
-                    <SearchInput style={searchinput} className="search_input" placeholder="Find workspace view" />
-                    <p className="title">Agile Project Management</p>
-                    <Link to="/works" className={selectedtabs === 0 ? "profile_links_selected" : "profile_links"}>
-                        <img src={map} alt="" height="11" width="14" />
-                        <p className="content">Epic Roadmap</p>
-                    </Link>
-                    <Link to="/board" className={selectedtabs === 1 ? "profile_links_selected" : "profile_links"}>
-                        <img src={board} alt="" height="15" width="14" />
-                        <p className="content">Board</p>
-                    </Link>
-                    <Link to="/list" className={selectedtabs === 7 ? "profile_links_selected" : "profile_links"}>
-                        <img src={table} alt="" height="12" width="14" />
-                        <p className="content">List</p>
-                    </Link>
-                    <Link to="/table" className={selectedtabs === 2 ? "profile_links_selected" : "profile_links"}>
-                        <img src={table} alt="" height="12" width="14" />
-                        <p className="content">Table</p>
-                    </Link>
-                    <Link to="/gantt_chart" className={selectedtabs === 3 ? "profile_links_selected" : "profile_links"}>
-                        <img src={timesheet} alt="" height="10" width="14" />
-                        <p className="content">Time Sheet</p>
-                    </Link>
-                    <Link to="/calendar" className={selectedtabs === 4 ? "profile_links_selected" : "profile_links"}>
-                        <img src={calendar} alt="" height="12" width="14" />
-                        <p className="content">Calendar</p>
-                    </Link>
-                    <Link to="/create_view" className={selectedtabs === 5 ? "profile_links_selected" : "profile_links"}>
-                        <img src={create_view} alt="" height="12" width="14" />
-                        <p className="content">Create View</p>
-                    </Link>
-                    <Link to="/settings" className={selectedtabs === 6 ? "profile_links_selected" : "profile_links"}>
-                        <img src={space_set} alt="" height="12" width="14" />
-                        <p className="content">Settings</p>
-                    </Link>
-                </div>
+            <div className="space_sidebar">
+                <SearchInput style={searchinput} className="search_input" placeholder="Find workspace view" />
+                <p className="title">Agile Project Management</p>
+                <Link to="/works" className={selectedtabs === 0 ? "profile_links_selected" : "profile_links"}>
+                    <img src={map} alt="" height="11" width="14" />
+                    <p className="content">Epic Roadmap</p>
+                </Link>
+                <Link to="/board" className={selectedtabs === 1 ? "profile_links_selected" : "profile_links"}>
+                    <img src={board} alt="" height="15" width="14" />
+                    <p className="content">Board</p>
+                </Link>
+                <Link to="/list" className={selectedtabs === 7 ? "profile_links_selected" : "profile_links"}>
+                    <img src={table} alt="" height="12" width="14" />
+                    <p className="content">List</p>
+                </Link>
+                <Link to="/table" className={selectedtabs === 2 ? "profile_links_selected" : "profile_links"}>
+                    <img src={table} alt="" height="12" width="14" />
+                    <p className="content">Table</p>
+                </Link>
+                <Link to="/gantt_chart" className={selectedtabs === 3 ? "profile_links_selected" : "profile_links"}>
+                    <img src={timesheet} alt="" height="10" width="14" />
+                    <p className="content">Time Sheet</p>
+                </Link>
+                <Link to="/calendar" className={selectedtabs === 4 ? "profile_links_selected" : "profile_links"}>
+                    <img src={calendar} alt="" height="12" width="14" />
+                    <p className="content">Calendar</p>
+                </Link>
+                <Link to="/create_view" className={selectedtabs === 5 ? "profile_links_selected" : "profile_links"}>
+                    <img src={create_view} alt="" height="12" width="14" />
+                    <p className="content">Create View</p>
+                </Link>
+                <Link to="/settings" className={selectedtabs === 6 ? "profile_links_selected" : "profile_links"}>
+                    <img src={space_set} alt="" height="12" width="14" />
+                    <p className="content">Settings</p>
+                </Link>
             </div>
         </div>
     );
