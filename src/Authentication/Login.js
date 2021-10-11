d5d9da06d65ab486fb6e9a26cb5cc724f3e71b25
sanigasani <86219056+sanigasani@users.noreply.github.com>
2021-10-11 06:03:46 -0700

    login

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Login.css";
 import path from "../assets/path.svg";
 import deski_white from "../assets/deski_white.svg";
@@ -6,6 +6,10 @@ import google_text from "../assets/google_text.svg";
 import { Pane, TextInput } from "evergreen-ui";
 import { Link, useHistory } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+
+import firebase from "../services/firebase";
+
+const auth = firebase.auth();
 
 const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minWidth: 951 });
@@ -21,16 +25,123 @@ const Mobile = ({ children }) => {
 };
 
 function Login() {
-    // const [value, setValue] = React.useState("");
+    
     const [incorrect, SetIncorrect] = React.useState(false);
     let history = useHistory();
 
-    // function abc(e) {
-    //     setValue(e.target.value);
-    //     if (value > 0) {
-    //         SetIncorrect(true);
-    //     }
-    // }
+ 
+    const [signupData, setSignUpData] = useState({
+        email: "",
+        password: "",
+       
+    });
+
+
+  
+
+    const handleChangeInput = (props) => (e) => {
+        if (props === "email") {
+            setSignUpData({
+                email: e.target.value,
+                password: signupData.password,
+              
+            });
+        } else if (props === "password") {
+            setSignUpData({
+                email: signupData.email,
+                password: e.target.value,
+              
+            });
+  
+    };
+}
+
+const handleSignup = (props) => {
+  
+     
+            var details = {
+                firebaseUid: "",
+                log_email: signupData.email,
+                type: "1",
+                log_password: signupData.password,
+               
+            };
+
+            var formBody = [];
+            for (var property in details) {
+                var encodedKey = encodeURIComponent(property);
+                var encodedValue = encodeURIComponent(details[property]);
+                formBody.push(encodedKey + "=" + encodedValue);
+            }
+            formBody = formBody.join("&");
+
+            fetch('http://18.116.203.74:6769/login', {
+                method: 'POST',
+                // mode: 'no-cors',
+          
+             headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+             },
+            
+             body: formBody
+           })
+        
+           .then((response) => response.json())
+           .then(response =>{ console.log(response)
+           if(response.log_email===response.email)
+           {  history.push("/workplace")}
+         } )
+         
+        }
+        const handleSignupWithGmail = (props) => {
+            const googleProvider = new firebase.auth.GoogleAuthProvider();
+            auth.signInWithPopup(googleProvider)
+                .then((res) => {
+                    console.log(res.user);
+    
+                    var displayName = res.user.displayName;
+                    var email = res.user.email;
+                    var firebaseUid = res.user.uid;
+                    console.log(displayName + " --- " + email + " --- " + firebaseUid);
+    
+                    var details = {
+                        firebaseUid: firebaseUid,
+                        email: email,
+                        type: "0",
+                        displayName: displayName,
+                    };
+    
+                    var formBody = [];
+                    for (var property in details) {
+                        var encodedKey = encodeURIComponent(property);
+                        var encodedValue = encodeURIComponent(details[property]);
+                        formBody.push(encodedKey + "=" + encodedValue);
+                    }
+                    formBody = formBody.join("&");
+    
+                    fetch('http://18.116.203.74:6769/login', {
+                        method: 'POST',
+                        mode:'no-cores',
+                        headers: {
+                          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                        },
+                        body: formBody
+                      }).then((response)=>{
+              
+                            console.log(response)
+                           
+                      })
+
+              
+                    }).catch((error) => {
+                      console.log(error.message)
+                    })
+              
+              
+                  }
+        React.useState(() => {});
+
+
 
     return (
         <div>
@@ -46,13 +157,14 @@ function Login() {
                             <h5>Your teams site</h5>
                             {incorrect && <p className="invalid">*The username or password is incorrect</p>}
                             <TextInput
-                                // onChange={(e) => setValue(e.target.value)}
+                             
                                 width={265}
                                 className="login_inputfield"
                                 borderRadius={2}
                                 borderColor="#dfe1e6"
                                 placeholder="Enter email"
-                                onInput={(e) => SetIncorrect(true)}
+                             
+                                onChange={handleChangeInput("email")}
                             />
                             <TextInput
                                 width={265}
@@ -61,11 +173,12 @@ function Login() {
                                 borderColor="#dfe1e6"
                                 borderRadius={2}
                                 placeholder="Password"
+                                onChange={handleChangeInput("password")}
                             />
                             <button
                                 className="login_continue"
                                 onClick={() => {
-                                    history.push("/workplace");
+                                    handleSignup("");
                                 }}
                             >
                                 Continue
@@ -74,7 +187,7 @@ function Login() {
                             <button
                                 className="login_btn"
                                 onClick={() => {
-                                    history.push("/workplace");
+                                    handleSignupWithGmail("");
                                 }}
                             >
                                 <img src={google_text} alt="" height="16" width="250" />
@@ -133,8 +246,9 @@ function Login() {
                                 className="mob_login_inputfield"
                                 borderRadius={2}
                                 borderColor="#dfe1e6"
-                                onInput={(e) => SetIncorrect(true)}
+                           
                                 placeholder="Enter email"
+                                onChange={handleChangeInput("email")}
                             />
                             <TextInput
                                 width={210}
@@ -143,11 +257,12 @@ function Login() {
                                 borderColor="#dfe1e6"
                                 borderRadius={2}
                                 placeholder="Password"
+                                onChange={handleChangeInput("password")}
                             />
                             <button
                                 className="mob_login_continue"
                                 onClick={() => {
-                                    history.push("/workplace");
+                                    handleSignup("");
                                 }}
                             >
                                 Continue
@@ -156,7 +271,7 @@ function Login() {
                             <button
                                 className="mob_login_btn"
                                 onClick={() => {
-                                    history.push("/workplace");
+                                    handleSignupWithGmail("");
                                 }}
                             >
                                 <img src={google_text} alt="" height="13" width="150" />
@@ -216,7 +331,8 @@ function Login() {
                                 borderRadius={2}
                                 borderColor="#dfe1e6"
                                 placeholder="Enter email"
-                                onInput={(e) => SetIncorrect(true)}
+                                
+                                onChange={handleChangeInput("email")}
                             />
                             <TextInput
                                 width={265}
@@ -225,11 +341,12 @@ function Login() {
                                 borderColor="#dfe1e6"
                                 borderRadius={2}
                                 placeholder="Password"
+                                onChange={handleChangeInput("password")}
                             />
                             <button
                                 className="login_continue"
                                 onClick={() => {
-                                    history.push("/workplace");
+                                    handleSignup("");
                                 }}
                             >
                                 Continue
@@ -238,7 +355,7 @@ function Login() {
                             <button
                                 className="login_btn"
                                 onClick={() => {
-                                    history.push("/workplace");
+                                    handleSignupWithGmail("");
                                 }}
                             >
                                 <img src={google_text} alt="" height="16" width="250" />
--- src/Authentication/Signup.js
+++ src/Authentication/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Signup.css";
+
 import path from "../assets/path.svg";
 import eye from "../assets/eye.svg";
 import deski_white from "../assets/deski_white.svg";
@@ -7,6 +8,10 @@ import google_text from "../assets/google_text.svg";
 import { Pane, TextInput } from "evergreen-ui";
 import { useHistory } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+
+import firebase from "../services/firebase";
+
+const auth = firebase.auth();
 
 const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minWidth: 951 });
@@ -30,6 +35,156 @@ function Signup() {
         setPasswordShown(passwordShown ? false : true);
     };
 
+    const [signupData, setSignUpData] = useState({
+        workEmail: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+    });
+
+
+  
+
+    const handleChangeInput = (props) => (e) => {
+        if (props === "wMail") {
+            setSignUpData({
+                workEmail: e.target.value,
+                password: signupData.password,
+                firstName: signupData.firstName,
+                lastName: signupData.lastName,
+            });
+        } else if (props === "fName") {
+            setSignUpData({
+                workEmail: signupData.workEmail,
+                password: signupData.password,
+                firstName: e.target.value,
+                lastName: signupData.lastName,
+            });
+        } else if (props === "lName") {
+            setSignUpData({
+                workEmail: signupData.workEmail,
+                password: signupData.password,
+                firstName: signupData.firstName,
+                lastName: e.target.value,
+            });
+        } else if (props === "password") {
+            setSignUpData({
+                workEmail: signupData.workEmail,
+                password: e.target.value,
+                firstName: signupData.firstName,
+                lastName: signupData.lastName,
+            });
+        }
+    };
+
+    const emailValidaton = (props) => {
+        const regex =
+            /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!props || regex.test(props) === false) {
+            return false;
+        }
+        return true;
+    };
+
+    const handleSignup = (props) => {
+        if (emailValidaton(signupData.workEmail)) {
+            if (signupData.firstName.length >7) {
+                // alert("Empty first name");
+            } else if (signupData.lastName.length >7) {
+                // alert("Empty last name");
+            } else if (signupData.password.length >16) {
+                // alert("Empty password");
+            } else {
+                var details = {
+                    firebaseUid: "",
+                    email: signupData.workEmail,
+                    type: "1",
+                    displayName: signupData.firstName,
+                    lastName: signupData.lastName,
+                    password: signupData.password,
+                };
+
+                var formBody = [];
+                for (var property in details) {
+                    var encodedKey = encodeURIComponent(property);
+                    var encodedValue = encodeURIComponent(details[property]);
+                    formBody.push(encodedKey + "=" + encodedValue);
+                }
+                formBody = formBody.join("&");
+
+                fetch('http://18.116.203.74:6769/signup', {
+                    method: 'POST',
+                    // mode: 'no-cors',
+              
+                 headers: {
+                   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+              
+                 },
+                
+                 body: formBody
+               })  
+               .then(
+                 (response) => {
+                    console.log(response);
+                    // history.push("/workplace")
+                 })
+            }}else {
+            // alert("not a valid email");
+        }
+    };
+    const handleSignupWithGmail = (props) => {
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(googleProvider)
+            .then((res) => {
+                console.log(res.user);
+
+                var displayName = res.user.displayName;
+                var email = res.user.email;
+                var firebaseUid = res.user.uid;
+                console.log(displayName + " --- " + email + " --- " + firebaseUid);
+
+                var details = {
+                    firebaseUid: firebaseUid,
+                    email: email,
+                    type: "0",
+                    displayName: displayName,
+                };
+
+                var formBody = [];
+                for (var property in details) {
+                    var encodedKey = encodeURIComponent(property);
+                    var encodedValue = encodeURIComponent(details[property]);
+                    formBody.push(encodedKey + "=" + encodedValue);
+                }
+                formBody = formBody.join("&");
+
+                fetch('http://18.116.203.74:6769/signup', {
+                    method: 'POST',
+                    mode:'no-cores',
+                    headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                    },
+                    body: formBody
+                  }).then((response)=>{
+                        
+          
+                        console.log(response)
+                  })
+          
+                 
+          
+                  
+          
+          
+          
+                }).catch((error) => {
+                  console.log(error.message)
+                })
+          
+          
+              }
+    React.useState(() => {});
+
     return (
         <div>
             <Desktop>
@@ -52,6 +207,7 @@ function Signup() {
                                     width={300}
                                     borderRadius={2}
                                     borderColor="#c5c5c5"
+                                    onChange={handleChangeInput("wMail")}
                                 />
                                 <p className="required">Password</p>
                                 <TextInput
@@ -59,16 +215,27 @@ function Signup() {
                                     borderColor="#c5c5c5"
                                     borderRadius={2}
                                     type={passwordShown ? "text" : "password"}
+                                    onChange={handleChangeInput("password")}
                                 />
                                 <div className="eye">
                                     <img src={eye} alt="" onClick={togglePasswordVisiblity} width="12" height="12" />
                                 </div>
                                 <div className="name">
                                     <p className="required">First Name:</p>
-                                    <TextInput width={140} borderRadius={2} borderColor="#c5c5c5" />
+                                    <TextInput
+                                        width={140}
+                                        borderRadius={2}
+                                        borderColor="#c5c5c5"
+                                        onChange={handleChangeInput("fName")}
+                                    />
                                     <div className="inline_text">
                                         <p className="required">Last Name:</p>
-                                        <TextInput width={140} borderRadius={2} borderColor="#c5c5c5" />
+                                        <TextInput
+                                            width={140}
+                                            borderRadius={2}
+                                            borderColor="#c5c5c5"
+                                            onChange={handleChangeInput("lName")}
+                                        />
                                     </div>
                                 </div>
                                 <p className="content">
@@ -96,7 +263,7 @@ function Signup() {
                                 <button
                                     className="signup_continue"
                                     onClick={() => {
-                                        history.push("/create_site");
+                                        handleSignup("");
                                     }}
                                 >
                                     Continue
@@ -107,7 +274,7 @@ function Signup() {
                             <button
                                 className="signup_btn"
                                 onClick={() => {
-                                    history.push("/create_site");
+                                    handleSignupWithGmail("");
                                 }}
                             >
                                 <img src={google_text} alt="" height="16" width="250" />
@@ -138,6 +305,7 @@ function Signup() {
                                     onInput={(e) => SetIncorrect(true)}
                                     borderRadius={2}
                                     borderColor="#c5c5c5"
+                                    onChange={handleChangeInput("wMail")}
                                 />
                                 <p className="required">Password</p>
                                 <TextInput
@@ -145,16 +313,27 @@ function Signup() {
                                     borderColor="#c5c5c5"
                                     borderRadius={2}
                                     type={passwordShown ? "text" : "password"}
+                                    onChange={handleChangeInput("password")}
                                 />
                                 <div className="eye">
                                     <img src={eye} alt="" onClick={togglePasswordVisiblity} width="12" height="12" />
                                 </div>
                                 <div className="name">
                                     <p className="required">First Name:</p>
-                                    <TextInput width={200} borderRadius={2} borderColor="#c5c5c5" />
+                                    <TextInput
+                                        width={200}
+                                        borderRadius={2}
+                                        borderColor="#c5c5c5"
+                                        onChange={handleChangeInput("fName")}
+                                    />
 
                                     <p className="required">Last Name:</p>
-                                    <TextInput width={200} borderRadius={2} borderColor="#c5c5c5" />
+                                    <TextInput
+                                        width={200}
+                                        borderRadius={2}
+                                        borderColor="#c5c5c5"
+                                        onChange={handleChangeInput("lName")}
+                                    />
                                 </div>
                                 <p className="content">
                                     By clicking below, you agree to the deski Cloud
@@ -179,7 +358,7 @@ function Signup() {
                                 <button
                                     className="mob_signup_continue"
                                     onClick={() => {
-                                        history.push("/create_site");
+                                        handleSignup("");
                                     }}
                                 >
                                     Continue
@@ -190,7 +369,7 @@ function Signup() {
                             <button
                                 className="mob_signup_btn"
                                 onClick={() => {
-                                    history.push("/create_site");
+                                    handleSignupWithGmail("");
                                 }}
                             >
                                 <img src={google_text} alt="" height="13" width="150" />
@@ -221,6 +400,7 @@ function Signup() {
                                     onInput={(e) => SetIncorrect(true)}
                                     borderRadius={2}
                                     borderColor="#c5c5c5"
+                                    onChange={handleChangeInput("wMail")}
                                 />
                                 <p className="required">Password</p>
                                 <TextInput
@@ -228,16 +408,27 @@ function Signup() {
                                     borderColor="#c5c5c5"
                                     borderRadius={2}
                                     type={passwordShown ? "text" : "password"}
+                                    onChange={handleChangeInput("password")}
                                 />
                                 <div className="eye">
                                     <img src={eye} alt="" onClick={togglePasswordVisiblity} width="12" height="12" />
                                 </div>
                                 <div className="name">
                                     <p className="required">First Name:</p>
-                                    <TextInput width={140} borderRadius={2} borderColor="#c5c5c5" />
+                                    <TextInput
+                                        width={140}
+                                        borderRadius={2}
+                                        borderColor="#c5c5c5"
+                                        onChange={handleChangeInput("fName")}
+                                    />
                                     <div className="inline_text">
                                         <p className="required">Last Name:</p>
-                                        <TextInput width={140} borderRadius={2} borderColor="#c5c5c5" />
+                                        <TextInput
+                                            width={140}
+                                            borderRadius={2}
+                                            borderColor="#c5c5c5"
+                                            onChange={handleChangeInput("lName")}
+                                        />
                                     </div>
                                 </div>
                                 <p className="content">
@@ -263,7 +454,7 @@ function Signup() {
                                 <button
                                     className="signup_continue"
                                     onClick={() => {
-                                        history.push("/create_site");
+                                        handleSignup("");
                                     }}
                                 >
                                     Continue
@@ -274,7 +465,7 @@ function Signup() {
                             <button
                                 className="signup_btn"
                                 onClick={() => {
-                                    history.push("/create_site");
+                                    handleSignupWithGmail("");
                                 }}
                             >
                                 <img src={google_text} alt="" height="16" width="250" />
